af7410ee0887da6fbd0d1848434eb79d1a1a265e
compoundbadr-tech <compoundbadr@gmail.com>
2025-12-11 18:47:18 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,144 +1,180 @@
-//   part 1 /*1*/ 
-const fs = require("fs");
-const { pipeline } = require("stream");
-const filePath = "data.txt";
-
-const stream = fs.createReadStream(filePath, {encoding: "utf-8", highWaterMark: 64 });
-stream.on("data", chunk => {
-    console.log("New chunk");
-    console.log(chunk);
-});
-
-stream.on("end", () => {
-    console.log("finshed reading file");
-    
-})
-   /*2*/
-const sourcePath = "./source.txt";
-const destPath = "./dest.txt";
-
-const readable = fs.createReadStream(sourcePath);
-const writable = fs.createWriteStream(destPath);
-readable.pipe(writable)
-writable.on("finish", () => {
-    console.log("file copied");
-})
-  /*3*/
-const zlib = require("zlib");
-const { pipe } = require("stream");
+//1
 const { error } = require("console");
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
-const input = "./data.txt";
-const output = "./data.txt.gz";
+app.use(express.json());
 
-const read = fs.createReadStream(input);
-const gzip = zlib.createGzip();
-const write = fs.createWriteStream(output);
+app.post("/user", (req,res)=>{
+    const {name, email} = req.body;
 
-pipeline(
-    read,
-    gzip,
-    write,
-    (err) => {
-        if (err) {
-            console.error("pipeline failed", err);
-        }else {
-            console.log("file compressed seccessfully");
-            
-        }
+    if(!name || !email) {
+        return res.status(400).json({error: "Name amd email are required"});
     }
-)
-//-----------------------------------------------------------------
-   /* part 3 */
-// 1 - The Node.js Event Loop is a system that allows Node.js
-//  to handle many tasks at the same time without blocking.
-//--------------------------------------------------------------
-// 2 - libuv is a C library that Node.js uses to handle
-// libuv gives Node.js the ability to perform tasks without blocking
-// libuv is the engine that powers Node.js’s asynchronous behavior.
-// libuv is the engine that powers Node.js’s asynchronous behavior.
-// libuv is the engine that powers Node.js’s asynchronous behavior.
-// Without libuv, Node.js would not be non-blocking.
-//-------------------------------------------------------------------
-// 3 - Node.js handles async operations by:
 
-// Using libuv to manage system operations
-
-// Running heavy tasks in a thread pool
-
-// Using the event loop to trigger callbacks when ready
-
-// Keeping the main JS thread free so it never blocks
-//------------------------------------------------------------
-// 4 - Call Stack	Executes JavaScript code
-// Event Queue	Stores async callbacks waiting to run
-// Event Loop	Moves tasks from queue → call stack when it's free
-//---------------------------------------------------------------------
-// 5 - Thread Pool	A set of background threads,
-//  used to handle heavy or blocking tasks ,
-// without blocking the main JavaScript thread.
-// Default Size:	4 threads
-// How to Change:	By setting the environment variable: UV_THREADPOOL_SIZE
-//-------------------------------------------------------------------------------------
-// 6 - Node.js handles blocking code synchronously on
-//  the main thread, while non-blocking code
-//  runs asynchronously in the background using the event loop and thread pool.
-//---------------------------------------------------------------------------------
-  // part 2 /*1*/ 
-const http = require("http");
-
-const userFile = "./users.json";
-
-function readUsers() {
-    const data = fs.readFileSync(userFile, "utf-8");
-    return JSON.parse(data);
-}
-function writeUsers(users) {
-    fs.writeFileSync(userFile, JSON.stringify(users, null, 2), "utf-8");
-}
-
-const server = http.createServer((req,res) => {
-    if (req.method === "POST" && req.url === "/user") {
-        let body = "";
-        req.on("data", chunk => {
-            body += chunk.toString();
-        });
-        req.on("end", () => {
-            if (!body) {
-                res.writeHead(400 , 
-                    {"content-type": "application/json"});
-                    return res.end(JSON.stringify({error: "Embty body"}));
-            }
-            let newUser = null;
-            newUser =JSON.parse(body);
-
-            if (newUser.name || newUser.email){
-                res.writeHead(400,
-                     {"content-type": "application/json"});
-                     return res.end(JSON.stringify({error: "Name and email are required"}));
-            }
-            const users = readUsers();
-            const exists = users.some(u => u.email === newUser.email);
-            if (exist) {
-                res.writeHead(409,
-                     {"content-type": "application/json"});
-                     return res.end(JSON.stringify({error : "Email already exists"}));
-            }
-            newUser.id = Date.now();
-            users.push(newUser);
-            writeUsers(users);
-
-            res.writeHead(201, {"content-type": "application/json"});
-            res.end(JSON.stringify({message : "user created" , user: newUser}));
-        });
+    let users = [];
+    try {
+        const data = fs.readFileSync("users.json", "utf-8" );
+        users = JSON.parse(data);
+    } catch (err) {
+        users = [];
     }
-    else {
-        res.writeHead(404, {"content-type": "application/json"});
-        res.end(JSON.stringify({error:"Not found"}));
+
+    const exists = users.find(u => u.email === email);
+    if (exists) {
+        return res.status(400).json({error: "email already exist"});
     }
+
+    const newUser = {id : Date.now(), name, email};
+    users.push(newUser);
+
+    fs.writeFileSync("users.json", JSON.stringify(users,null,2));
+
+    return res.status(201).json({
+        message: "user added",
+        user: newUser
+    });
 });
 
-server.listen(3000, () => {
-    console.log("server running");
+app.listen(3000, ()=>{
+    console.log("server running in port 3000");
     
 });
+//----------------------------------------------------------
+//2
+app.use(express.json());
+app.patch("/user/:id", (req,res)=>{
+    const userId = req.params.id;
+    const {name, email, age} = req.body;
+
+    let users = [];
+    try {
+        const data = fs.readFileSync("users.json", "utf-8");
+        users = JSON.parse(data);
+    } catch (err) {
+        return res.status(500).json({error: "error reaading users file"});
+    }
+
+    const userIndex = users.findIndex(u => u.id == userId);
+
+    if (userIndex === -1){
+        return res.status(404).json({error: "user not found"});
+    }
+    const user = users[userIndex];
+
+    if (name) users[userIndex].name = name;
+    if (email) users[userIndex].email = email;
+    if (age) users[userIndex].age = age;
+
+    users[userIndex] = user;
+    fs.writeFileSync("users.json", JSON.stringify(users, null,2));
+    return res.json({
+        message: "user updated",
+        user: users[userIndex]
+    });
+});
+//--------------------------------------------------------------------------
+//3
+
+app.use(express.json());
+
+app.delete("/user/:id", (req, res) => {
+    const paramId = req.params.id;
+    const bodyId = req.body.id;
+
+    const userId = paramId || bodyId;
+
+    if (!userId) {
+        return res.status(400).json({ error: "User ID is required" });
+    }
+
+     fs.readFile("users.json", "utf8", (err, data) => {
+        if (err) return res.status(500).json({ error: "Failed to read file" });
+
+        let users = JSON.parse(data);
+
+        const newUsers = users.filter(u => u.id != userId);
+
+        if (newUsers.length === users.length) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        fs.writeFile("users.json", JSON.stringify(newUsers, null, 2), (err) => {
+            if (err) return res.status(500).json({ error: "Failed to write file" });
+
+            return res.json({
+                message: "User deleted successfully",
+                deletedId: userId
+            });
+        });
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+});
+//--------------------------------------------------------
+//4
+app.get("/user/getByName", (req,res)=>{
+    const name = req.query.name;
+
+    if (!name) {
+        return res.status(400).json({error: "Name is required"});
+    }
+    fs.readFile("users.json", "utf-8", (err, data)=>{
+        if (err) return res.status(500).json({error:"failed to read file"});
+        const users = JSON.parse(data);
+        const user = users.find(u => u.name.toLowerCase() === name.toLowerCase());
+
+        if(!user) {
+            return res.status(404).json({error:"user not found"})
+        }
+        return res.json(user);
+    });
+});
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+});
+//-------------------------------------------------
+//5
+app.get('/user', (req,res)=>{
+    const data = fs.readFileSync('./users.json', "utf-8");
+    const users = JSON.parse(data);
+    res.json(users);
+})
+app.listen(3000, () =>{
+    console.log('server is running on port 3000');
+    
+});
+//-----------------------------------------------------
+//6
+app.get('/user/filter', (req, res) => {
+    const { minAge } = req.query;
+    const data = fs.readFileSync('./users.json', 'utf8');
+    const users = JSON.parse(data);
+
+    if (!minAge) {
+        return res.status(400).json({ error: "minAge is required" });
+    }
+
+    const filtered = users.filter(u => u.age >= Number(minAge));
+    res.json(filtered);
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+//--------------------------------------------------
+//7
+app.get('/user/:id', (req,res)=>{
+    const userId = Number(req.params.id);
+    const data = fs.readFileSync('./users.json', 'utf-8');
+    const users = JSON.parse(data);
+    const user = users.find(u => u.id === userId);
+    
+    if(!user) {
+        return res.status(404).json({error:"user not found"});
+    }
+    res.json(user);
+})
